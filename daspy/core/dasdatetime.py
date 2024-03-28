866f42f7921c6426b8e3f89951624cb4aa2613f0
Minzhe Hu <hmz2018@ustc.mail.edu.cn>
2024-03-28 23:00:40 +0800

    update DASDateTime.__sub__

--- daspy/core/dasdatetime.py
+++ daspy/core/dasdatetime.py
@@ -18,11 +18,15 @@ class DASDateTime(datetime):
         return super().__add__(other)
 
     def __sub__(self, other):
-        if isinstance(other, datetime):
+        if isinstance(other, (tuple, list, np.ndarray)):
+            out = []
+            for t in other:
+                out.append(self - t)
+            return out
+        elif isinstance(other, datetime):
             return (super().__sub__(other)).total_seconds()
         elif not isinstance(other, timedelta):
             other = timedelta(seconds=other)
-
         return super().__sub__(other)
 
     def convert_to_datetime(self):
