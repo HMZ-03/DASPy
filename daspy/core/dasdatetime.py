e87b545d89e1e0fc69eb1a4401089e8ac40074ac
Minzhe Hu <hmz2018@ustc.mail.edu.cn>
2024-03-28 22:52:03 +0800

    update DASDateTime.__add__

--- daspy/core/dasdatetime.py
+++ daspy/core/dasdatetime.py
@@ -1,15 +1,20 @@
 # Purpose: Module for handling DASDateTime objects.
 # Author: Minzhe Hu
-# Date: 2024.3.14
+# Date: 2024.3.28
 # Email: hmz2018@mail.ustc.edu.cn
+import numpy as np
 from datetime import datetime, timedelta
 
 
 class DASDateTime(datetime):
     def __add__(self, other):
-        if not isinstance(other, timedelta):
+        if isinstance(other, (tuple, list, np.ndarray)):
+            out = []
+            for t in other:
+                out.append(self + t)
+            return out
+        elif not isinstance(other, timedelta):
             other = timedelta(seconds=other)
-
         return super().__add__(other)
 
     def __sub__(self, other):
