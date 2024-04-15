5f148b82c6bed5c359a7d6277dea0c96c33e2c63
Minzhe Hu <hmz2018@ustc.mail.edu.cn>
2024-04-15 20:15:10 +0800

    Added handling of time zones

--- daspy/core/dasdatetime.py
+++ daspy/core/dasdatetime.py
@@ -1,8 +1,9 @@
 # Purpose: Module for handling DASDateTime objects.
 # Author: Minzhe Hu
-# Date: 2024.4.11
+# Date: 2024.4.15
 # Email: hmz2018@mail.ustc.edu.cn
-import numpy as np
+from typing import Iterable
+import time
 from datetime import datetime, timedelta, timezone
 
 
@@ -10,8 +11,14 @@ utc = timezone.utc
 
 
 class DASDateTime(datetime):
+
+    _tz_str = time.strftime('%z', time.localtime())
+    _tz_h = int(_tz_str[:3])
+    _tz_m = int(_tz_str[0] + _tz_str[3:5])
+    local_tz = timezone(timedelta(hours=_tz_h, minutes=_tz_m))
+
     def __add__(self, other):
-        if isinstance(other, (tuple, list, np.ndarray)):
+        if isinstance(other, Iterable):
             out = []
             for t in other:
                 out.append(self + t)
@@ -21,13 +28,19 @@ class DASDateTime(datetime):
         return super().__add__(other)
 
     def __sub__(self, other):
-        if isinstance(other, (tuple, list, np.ndarray)):
+        if isinstance(other, Iterable):
             out = []
             for t in other:
                 out.append(self - t)
             return out
         elif isinstance(other, datetime):
-            return (super().__sub__(other)).total_seconds()
+            if self.tzinfo and not other.tzinfo:
+                subtracted = other.replace(tzinfo=DASDateTime.local_tz)
+            elif not self.tzinfo and other.tzinfo:
+                return - (other - self)
+            else:
+                subtracted = other
+            return (super().__sub__(subtracted)).total_seconds()
         elif not isinstance(other, timedelta):
             other = timedelta(seconds=other)
         return super().__sub__(other)
