86de87f41d40b0e2d6c5006ae6527ea4a733b593
Minzhe Hu <hmz2018@ustc.mail.edu.cn>
2024-06-08 17:13:16 +0800

    update read_json

--- daspy/core/read.py
+++ daspy/core/read.py
@@ -1,10 +1,11 @@
 # Purpose: Module for reading DAS data.
 # Author: Minzhe Hu
-# Date: 2024.4.25
+# Date: 2024.6.8
 # Email: hmz2018@mail.ustc.edu.cn
 # Modified from
 # https://github.com/RobbinLuo/das-toolkit/blob/main/DasTools/DasPrep.py
 import warnings
+import json
 import pickle
 import numpy as np
 import h5py
@@ -15,15 +16,15 @@ from daspy.core.section import Section
 from daspy.core.dasdatetime import DASDateTime
 
 
-def read(fname=None, output_type='Section', **kwargs):
+def read(fname=None, output_type='section', **kwargs):
     """
     :param fname: str or pathlib.PosixPath. Path of DAS data file.
-    :param output_type: str. 'Section' means output an instances of
-        daspy.Section, 'array' means output numpy.array for data and a
-        dictionary for metadata
+    :param output_type: str. 'Section' means return an instance of
+        daspy.Section, 'array' means return numpy.array for data and a
+        dictionary for metadata.
     :param ch1: int. The first channel required.
     :param ch2: int. The last channel required (not included).
-    :return: An instances of daspy.Section, or numpy.array for data and a
+    :return: An instance of daspy.Section, or numpy.array for data and a
         dictionary for metadata.
     """
     fun_map = {'pkl': _read_pkl, 'tdms': _read_tdms, 'h5': _read_h5,
@@ -34,13 +35,13 @@ def read(fname=None, output_type='Section', **kwargs):
         ftype = fname.lower().split('.')[-1]
         data, metadata = fun_map[ftype](fname, **kwargs)
 
-    if output_type == 'Section':
+    if output_type.lower() == 'section':
         if metadata['dx'] is None:
             print('Please set Section.dx manually.')
         if metadata['fs'] is None:
             print('Please set Section.fs manually.')
         return Section(data.astype(float), **metadata)
-    elif output_type == 'array':
+    elif output_type.lower() == 'array':
         return data, metadata
 
 
@@ -185,3 +186,74 @@ def _read_segy(fname, **kwargs):
         metadata = {'fs': fs, 'dx': None, 'start_channel': ch1}
 
         return data, metadata
+
+
+def read_json(fname, output_type='dict'):
+    """
+    :param fname: str or pathlib.PosixPath. Path of json file.
+    :param output_type: str. 'dict' means return a dictionary, and 'Section'
+        means return a empty daspy.Section instance with metadata.
+    :return: A dictionary of metadata or an instance of daspy.Section without
+        data.
+    """
+    with open(fname, 'r') as fcc_file:
+        headers = json.load(fcc_file)
+    if output_type.lower() == 'dict':
+        return headers
+    elif output_type.lower() == 'section':
+        if len(headers['Overview']['Interrogator']) > 1:
+            case_type = 'Multiple interrogators, single cable'
+            sec_num = len(headers['Overview']['Interrogator'])
+            sec = []
+            for interrogator in headers['Overview']['Interrogator']:
+                nch = interrogator['Acquisition'][0]['Attributes']\
+                    ['number_of_channels']
+                data = np.zeros((nch, 0))
+                dx = interrogator['Acquisition'][0]['Attributes']\
+                    ['spatial_sampling_interval']
+                fs = interrogator['Acquisition'][0]['Attributes']\
+                    ['acquisition_sample_rate']
+                gauge_length = interrogator['Acquisition'][0]\
+                    ['Attributes']['gauge_length']
+                sec.append(Section(data, dx, fs, gauge_length=gauge_length,
+                                   headers=headers))
+        elif len(headers['Overview']['Interrogator'][0]['Acquisition']) > 1:
+            case_type = 'Active survey'
+            sec_num = len(headers['Overview']['Interrogator'][0]['Acquisition'])
+            sec = []
+            for acquisition in headers['Overview']['Interrogator'][0]\
+                ['Acquisition']:
+                nch = acquisition['Attributes']['number_of_channels']
+                data = np.zeros((nch, 0))
+                dx = acquisition['Attributes']['spatial_sampling_interval']
+                fs = acquisition['Attributes']['acquisition_sample_rate']
+                gauge_length = acquisition['Attributes']['gauge_length']
+                sec.append(Section(data, dx, fs, gauge_length=gauge_length,
+                                   headers=headers))
+        else:
+            sec_num = 1
+            if len(headers['Overview']['Cable']) > 1:
+                case_type = 'Single interrogators, multiple cable'
+            else:
+                env = headers['Overview']['Cable'][0]['Attributes']['cable_environment']
+                if env == 'trench':
+                    case_type = 'Direct buried'
+                elif env == 'conduit':
+                    case_type = 'Dark fiber'
+                elif env in ['wireline', 'outside borehole casing']:
+                    case_type = 'Borehole cable'
+            nch = headers['Overview']['Interrogator'][0]['Acquisition'][0]\
+                ['Attributes']['number_of_channels']
+            dx = headers['Overview']['Interrogator'][0]['Acquisition'][0]\
+                ['Attributes']['spatial_sampling_interval']
+            fs = headers['Overview']['Interrogator'][0]['Acquisition'][0]\
+                ['Attributes']['acquisition_sample_rate']
+            gauge_length = headers['Overview']['Interrogator'][0]\
+                ['Acquisition'][0]['Attributes']['gauge_length']
+            data = np.zeros((nch, 0))
+            sec = Section(data, dx, fs, gauge_length=gauge_length,
+                          headers=headers)
+
+        print(f'For case of {case_type}, create {sec_num} empty daspy.Section '
+              'instance(s)')
+        return sec
