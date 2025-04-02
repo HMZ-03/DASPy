07ee32b2080e0e0468f21170cc75ce44e1e7f576
Minzhe Hu <hmz2018@ustc.mail.edu.cn>
2025-04-02 17:11:35 +0800

    1.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='DASPy-toolbox', version='1.1.5',
+    name='DASPy-toolbox', version='1.1.4',
     description=(
         'DASPy is an open-source project dedicated to provide a python package '
         'for DAS (Distributed Acoustic Sensing) data processing, which '
